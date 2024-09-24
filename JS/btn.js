7f87642218acf557f3daa5a75c07abc5fb59bfee
Muhammad-Piyas <piyas62552@gmail.com>
2024-09-24 19:38:12 +0600

    button dynamic done

---
+++ JS/btn.js
@@ -0,0 +1,24 @@
+const donationBtn = document.getElementById("donationBtn");
+const historyBtn = document.getElementById("historyBtn");
+
+//History Button
+historyBtn.addEventListener("click", function () {
+  historyBtn.classList.remove("bg-gray-200", "hover:bg-gray-300");
+
+  historyBtn.classList.add("bg-lime-300", "hover:bg-lime-400");
+
+  donationBtn.classList.add("bg-gray-200", "hover:bg-gray-300");
+
+  donationBtn.classList.remove("bg-lime-300", "hover:bg-lime-400");
+});
+
+//Donation Button
+donationBtn.addEventListener("click", function () {
+  donationBtn.classList.remove("bg-gray-200", "hover:bg-gray-300");
+
+  donationBtn.classList.add("bg-lime-300", "hover:bg-lime-400");
+
+  historyBtn.classList.add("bg-gray-200", "hover:bg-gray-300");
+
+  historyBtn.classList.remove("bg-lime-300", "hover:bg-lime-400");
+});
